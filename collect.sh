0332ac6c26e9e451d4283bf48e93383029a225d4
gaston <gastonmazzei95@gmail.com>
2021-12-28 13:04:10 +0100

    completed the kernel pipe :-)

--- collect.sh
+++ collect.sh
@@ -5,16 +5,17 @@ sudo su -c utils/configure_kerneltracer.sh
 sudo su -c utils/activate_kerneltracer.sh
 
 # Pipe the kernel trace while collecting user's tags via a python script
-#(trap 'kill 0' SIGINT; sudo su -c utils/pipe_kerneltracer.sh & python3 pythonscripts/time_fetcher.py)
+(
+sudo su -c utils/pipe_kerneltracer.sh &
+echo "LASTPID=$!" > utils/lastpid.temp ;
+python3 pythonscripts/time_fetcher.py;
+)
 
-# Spawn a shell with python
-#python3 pythonscripts/time_fetcher.py
-# Store the results
-#sudo cp /sys/kernel/debug/tracing/trace raw_data/trace
 
-# Deactivate tracer and restore defaults
+# Remove the pipe and stop the kernel tracer
+source utils/lastpid.temp
+kill $LASTPID
 sudo  su -c utils/deactivate_kerneltracer.sh
 
-# Let the trace defaults restore themselves and destroy the backup :-)
-#sudo su -c utils/reconfigure_kerneltracer.sh
-rm utils/trace_options.backup
+# End
+echo "End of the script! :-)"
